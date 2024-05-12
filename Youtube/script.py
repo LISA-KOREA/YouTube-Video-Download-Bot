0e764da960681cff78f8fa1964d2e1ae2412d9c6
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2024-05-12 20:34:52 +0530

    Create script.py

---
+++ Youtube/script.py
@@ -0,0 +1,15 @@
+class Translation(object):
+
+    START_TEXT = '**Hello,** {}!\n\n**Send me the YouTube link of the video you want to upload**' 
+
+    ABOUT_TXT = """
+╭───────────⍟
+├📛 **My Name** : [YouTube Video Uploader Bot](https://t.me/YouTubeUploaderOneBot)
+├📢 **Framework** : [Pyrogram 2.0.106](https://docs.pyrogram.org/)
+├💮 **Language** : [Python 3.12.3](https://www.python.org)
+├👥 **Support Group** : [NT BOTS SUPPORT](https://t.me/NT_BOTS_SUPPORT)
+├🥏 **Channel** : [NT BOT CHANNEL](https://t.me/NT_BOT_CHANNEL)
+├⛲ **Source** : [Click](https://github.com/LISA-KOREA/YouTube-Video-Download-Bot)
+├🎓 **Developer** : [LISA_FAN_LK](https://t.me/LISA_FAN_LK)
+╰───────────────⍟
+"""
