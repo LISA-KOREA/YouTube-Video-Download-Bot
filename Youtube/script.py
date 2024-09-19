7d30768351f20551427cbc64be69e848160fb3d4
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2024-09-19 12:35:21 +0530

    Update script.py

--- Youtube/script.py
+++ Youtube/script.py
@@ -5,8 +5,8 @@ class Translation(object):
     ABOUT_TXT = """
 ╭───────────⍟
 ├📛 **My Name** : [YouTube Video Uploader Bot](https://t.me/YouTubeUploaderOneBot)
-├📢 **Framework** : [Pyrogram 2.0.106](https://docs.pyrogram.org/)
-├💮 **Language** : [Python 3.12.3](https://www.python.org)
+├📢 **Framework** : [PyroFork 2.3.46](https://docs.pyrogram.org/)
+├💮 **Language** : [Python 3.12.6](https://www.python.org)
 ├👥 **Support Group** : [NT BOTS SUPPORT](https://t.me/NT_BOTS_SUPPORT)
 ├🥏 **Channel** : [NT BOT CHANNEL](https://t.me/NT_BOT_CHANNEL)
 ├⛲ **Source** : [Click Here](https://github.com/LISA-KOREA/YouTube-Video-Download-Bot)
