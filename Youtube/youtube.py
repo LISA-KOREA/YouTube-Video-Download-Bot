7800a7995aea2a8f72ef92cf786e2400e05d7a6b
JOBIN JUSTIN <82756356+TG-SORRY@users.noreply.github.com>
2025-09-07 14:30:20 +0530

    Bug fix

--- Youtube/youtube.py
+++ Youtube/youtube.py
@@ -68,7 +68,7 @@ async def handle_download_button(client, callback_query):
             info_dict = ydl.extract_info(youtube_link, download=False)
             video_id = info_dict.get('id')
             title = info_dict.get('title')
-            duration = info.get("duration", 0)
+            duration = info_dict.get("duration", 0)
 
             if title and video_id:
                 ydl.download([youtube_link])
